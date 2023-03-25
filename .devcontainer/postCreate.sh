7f1f38fe5936bb6a1cb97f9a6971badc1638c221
Lars Kruse <lakruzz@inc-inc.dk>
2023-03-25 10:14:07 +0100

    Added mysql

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,3 +3,6 @@
 curl -LO https://github.com/planetscale/cli/releases/download/v0.131.0/pscale_0.131.0_linux_amd64.deb
 sudo dpkg -i pscale_0.131.0_linux_amd64.deb
 rm pscale_0.131.0_linux_amd64.deb
+
+sudo apk add mysql
+sudo apk add mysql-client
